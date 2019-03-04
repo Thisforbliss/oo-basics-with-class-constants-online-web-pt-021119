3b59187a26380dd685f48944d8321dfa7ec9783f
Malcolm Leake <echelon1089@gmail.com>
2019-03-04 04:34:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,11 +5,9 @@ class Shoe
  BRANDS= []
   def initialize(brand)
     @brand = brand
-   if !(BRANDS.include?(brand)) 
-     BRANDS << @brand 
+  
+     BRANDS << @brand if !(BRANDS.include?(brand))
 
-      
-   end  
   end
   
   
